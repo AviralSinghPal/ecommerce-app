905c26a52c61f8321e41181ca53bcb79d3975b0f
AviralSinghPal <aviralpal00007@gmail.com>
2023-07-21 00:19:55 +0530

    added logo and product card

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,35 +1,40 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../utils/AuthContext';
-import './Header.css'; 
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../utils/AuthContext";
+import "./Header.css";
+import logoimg from "../img/logo.png";
 const Header = () => {
-  const { isLoggedin } = useContext(AuthContext); 
+  const { isLoggedin } = useContext(AuthContext);
 
   return (
-    <header className="header"> 
+    <header className="header">
       <div className="container">
-        <h1 className="logo">Glamazon</h1>
+        <div className="logo-container">
+          <img className="logo" src={logoimg} alt="Glamazon Logo" />
+          <h1 className="logo-name">Glamazon</h1>
+        </div>
+
         {isLoggedin ? (
-          <nav className="nav"> 
-            <ul className="ul"> 
-              <li className="li"> 
+          <nav className="nav">
+            <ul className="ul">
+              <li className="li">
                 <Link to="/">Home</Link>
               </li>
-              <li className="li"> 
+              <li className="li">
                 <Link to="/products">Products</Link>
               </li>
-              <li className="li"> 
+              <li className="li">
                 <Link to="/cart">Cart</Link>
               </li>
-              <li className="li"> 
+              <li className="li">
                 <Link to="/logout">Logout</Link> {/* Add a logout link */}
               </li>
             </ul>
           </nav>
         ) : (
-          <nav className="nav"> 
-            <ul className="ul"> 
-              <li className="li"> 
+          <nav className="nav">
+            <ul className="ul">
+              <li className="li">
                 <Link to="/login">Login</Link>
               </li>
             </ul>
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,31 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import ProductCard from './ProductCard';
+import './Home.css'; // Import the CSS file
 
 const Home = () => {
-  const [users, setUsers] = useState([]);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    // Fetch users data from the API
-    const fetchUsers = async () => {
+    // Fetch products data from the API
+    const fetchProducts = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/api/users/');
-        setUsers(response.data);
+        const response = await axios.get('http://localhost:5000/api/products/');
+        setProducts(response.data);
       } catch (error) {
-        console.error('Failed to fetch users:', error.message);
+        console.error('Failed to fetch products:', error.message);
       }
     };
 
-    fetchUsers();
+    fetchProducts();
   }, []);
 
   return (
-    <div>
+    <div className="home-container">
       <h1>Welcome to the Home Page</h1>
-      <h2>User List:</h2>
-      <ul>
-        {users.map((user) => (
-          <li key={user._id}>
-            <strong>{user.username}</strong> - {user.email}
+
+      <h2>Here are some Next Gen products</h2>
+      <ul className="product-list">
+        {products.map((product) => (
+          <li key={product._id} className="product-card">
+            <ProductCard
+              name={product.name}
+              description={product.description}
+              price={product.price}
+            />
           </li>
         ))}
       </ul>
